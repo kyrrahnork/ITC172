3193d5bf0b453e26cfbf4d4637076c1dc12a09a6
Kyrrah Nork <kyrrahnork@gmail.com>
2017-01-09 12:05:14 -0800

    Add files via upload

---
+++ Assignment1/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>tip calculator</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Tip Calculator</h1>
+    <div>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Enter Meal Amount: "></asp:Label>
+            <!--renamed because I will refer to textbox in code-->
+            <asp:TextBox ID="MealTextbox" runat="server"></asp:TextBox><br /><br />
+            Select Tip Amount Below:
+            <asp:RadioButtonList ID="TipPercentsRadioButtonList" runat="server"></asp:RadioButtonList>
+            <asp:TextBox ID="OtherTextBox" runat="server"></asp:TextBox><br /><br />
+            <asp:Button ID="SubmitButton" runat="server" Text="Calculate" OnClick="SubmitButton_Click" />
+            <asp:Label ID="ResultLabel" runat="server" Text=""></asp:Label>
+        </p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Assignment1/Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            string[] tipPercent = { "10%", "15%", "20%", "Other" };
+            TipPercentsRadioButtonList.DataSource = tipPercent;
+            TipPercentsRadioButtonList.DataBind();
+        }
+    }
+
+    protected void SubmitButton_Click(object sender, EventArgs e)
+    {
+        GetInfo();
+    }
+
+    protected void GetInfo()
+    {
+        Tip tip = new Tip();
+        tip.MealAmount = double.Parse(MealTextbox.Text);
+        if(OtherTextBox.Text == "")
+        { tip.TipPercent = 0;
+            foreach(ListItem item in TipPercentsRadioButtonList.Items)
+            {
+               
+                if (item.Selected)
+                {
+                    if (item.Text.Equals("10%"))
+                    {
+                        tip.TipPercent = .1;
+                    }
+                    else if (item.Text.Equals("15%"))
+                    {
+                        tip.TipPercent = .15;
+                    }
+                    else if (item.Text.Equals("20%"))
+                    {
+                        tip.TipPercent = .2;
+                    }   
+                }
+            }
+        }
+        else
+        {
+            tip.TipPercent = double.Parse(OtherTextBox.Text);       
+        }
+
+        ResultLabel.Text = "Amount: " + tip.MealAmount.ToString() + "<br/>" +
+            "Tip: " + tip.CalculateTip().ToString("$#,##0.00") + "<br/>" +
+            "Tax: " + tip.CalculateTax().ToString("$#,##0.00") + "<br/>" +
+            "Tax: " + tip.CalculateTotal().ToString("$#,##0.00");
+    }
+}
