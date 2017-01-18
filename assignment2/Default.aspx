19938e79c23d9b51211de03f5f6867d63ae3f048
Kyrrah Nork <kyrrahnork@gmail.com>
2017-01-18 10:59:51 -0800

    Add files via upload

---
+++ assignment2/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Event Calendar</title>
+    <link rel="stylesheet" href="StyleSheet.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="content">
+        <h1>Calendar of Events</h1>
+        <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar><br />
+        <asp:Button ID="Button1" runat="server" Text="Click to check for events" OnClick="Button1_Click" /><br /><br />
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ assignment2/Default.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string[,] calEvent = new string[5, 2];
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        calEvent[0, 0] = "Kyrrah's Birthday";
+        calEvent[0, 1] = "1/1/2017";
+        calEvent[1, 0] = "Christmas Party";
+        calEvent[1, 1] = "12/24/2017";
+        calEvent[2, 0] = "New Year Party";
+        calEvent[2, 1] = "12/31/2016";
+        calEvent[3, 0] = "Tutoring";
+        calEvent[3, 1] = "1/14/2017";
+        calEvent[4, 0] = "Work at Information Central";
+        calEvent[4, 1] = "1/14/2017";
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        GetEvents();
+    }
+
+    private void GetEvents()
+    {
+        Label1.Text = "";
+        if (Calendar1.SelectedDate > DateTime.MinValue)
+        {
+            string calendarDateTime = Calendar1.SelectedDate.ToShortDateString();
+            for (int i = 0; i < calEvent.GetLength(0); i++)
+            {
+                if (calEvent[i, 1].Equals(calendarDateTime))
+                {
+                    Label1.Text += calEvent[i, 0] + "<br />";
+                }
+            }
+        }
+        if (Label1.Text.Length == 0)
+        {
+            Label1.Text = "No events found.";
+        }
+    }
+
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        //GetEvents();
+        // debug for array length
+        //Label1.Text = "Total length: " + calEvent.Length + "<br />Dimension 0: " + calEvent.GetLength(0) + "<br />Dimension 1: " + calEvent.GetLength(1);
+    }
+}
