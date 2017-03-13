13bad12a84fb2cf7b309a4278feff4f00f8f2937
Kyrrah Nork <kyrrahnork@gmail.com>
2017-03-13 10:09:38 -0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NorkAssignment7.Startup))]
+namespace NorkAssignment7
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
